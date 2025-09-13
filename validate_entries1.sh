52cc203e27eeaafb284147369a5b658d2376296b
ndinh1 <ndinh19019@gmail.com>
2025-09-13 10:54:41 -0500

    validate_entries1.sh

--- validate_entries1.sh
+++ validate_entries1.sh
@@ -2,7 +2,7 @@
 
 validate_file(){
         if [[ ! -f "$1" ]]; then
-                echo "Error: file not found"
+                echo "Error: '$1' file not found"
                 exit 1
         fi
 }
@@ -12,26 +12,27 @@ extract_entries() {
         level="$2"
         validate_file "$log_file"
         echo "Extracting $level entries from $log_file"
-        grep "[$level]" "$log_file"
+        grep "\[$level\]" "$log_file"
 }
 
 count_entries(){
         log_file="$1"
-        level="$2"
-        validate_file="$level"
-        echo "Summary of log levels : "
+        validate_file "$log_file"
+        echo "Summary of log levels in $log_file:"
         echo "ERROR: $(grep -c "[ERROR]" "$log_file")"
         echo "WARNING: $(grep -c "[WARNING]" "$log_file")"
         echo "INFO: $(grep -c "[INFO]" "$log_file")"
 }
 
-
-if [[ $# -eq 1 ]]; then
+#Main 
+if [[ $# -eq 2 ]]; then
+        log_file="$1"
+        level="$2"
+        extract_entries "$log_file" "$level"
+elif [[ $# -eq 1 ]]; then
         log_file="$1"
         count_entries "$log_file"
-        echo
-        extract_entries "$log_file" "ERROR"
 else
-        echo "Usage $0 <logfile>"
+        echo "Usage $0 <logfile> [ERROR|WARNING|INFO]"
         exit 1
 fi
